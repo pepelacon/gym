f919df9b3e0a675982489766c0ba17967608b6d0
Daniel <pepelacon@gmail.com>
2023-12-13 09:42:24 -0500

    add statistics

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -5,6 +5,7 @@ import Button from '../../ui/button/Button'
 import Layout from '../../layout/Layout'
 
 import styles from './Home.module.scss'
+import Statistics from '../profile/statistics/Statistics'
 
 function Home() {
 	const navigate = useNavigate()
@@ -13,7 +14,7 @@ function Home() {
 		<Layout bgImage='/images/home-bg.jpg'>
 			<Button clickHandler={() => navigate('/new-workout')}>New</Button>
 			<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
-			{/* TODO: Counters */}
+			<Statistics />
 		</Layout>
 	)
 }
